e59bcb40b2896c4f68ae6929ea584415c077e183
Esther Nila <esthernilamiranda@gmail.com>
2026-07-02 14:32:06 -0600

    Commit Esther. Se crea función changeColor

--- ad-l3-1.0/script.js
+++ ad-l3-1.0/script.js
@@ -1,4 +1,13 @@
 function changeColor (elemento){
-    elemento.style.color = "red";
-    
+    const aleatorio = Math.random();
+    let color;
+    if (aleatorio <= 0.33)
+        color = "red";
+    else if (aleatorio <= 0.66)
+        color = "blue";
+    else
+        color = "green";
+
+    elemento.style.color = color;
 }
+
